fd37055d07047e08bdf3fcede7bf38c1b8f8a1bf
Naoya Ito <i.naoya@gmail.com>
2013-02-06 17:32:21 +0900

    initial commit

---
+++ lib/Text/LTSV.pm
@@ -0,0 +1,32 @@
+package Text::LTSV;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Text::LTSV -
+
+=head1 SYNOPSIS
+
+  use Text::LTSV;
+
+=head1 DESCRIPTION
+
+Text::LTSV is
+
+=head1 AUTHOR
+
+Naoya Ito E<lt>i.naoya@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
